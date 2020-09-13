8181c137a51fa5e18f137a3354a75cb4d95191ed
thorvaldUA <thorvald.ua@gmail.com>
2020-09-13 13:54:52 +0300

    publish to master

---
+++ build/precache-manifest.d5762f4af808f4dd0d4648eea8b8d33a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4235e737fffd8ea50c2258af92e306f7",
+    "url": "/clego-deploy/index.html"
+  },
+  {
+    "revision": "9b4c7946c4838c8036fd",
+    "url": "/clego-deploy/static/css/main.2c672629.chunk.css"
+  },
+  {
+    "revision": "edc97cab40c6b8f02540",
+    "url": "/clego-deploy/static/js/2.51d70eaa.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/clego-deploy/static/js/2.51d70eaa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b4c7946c4838c8036fd",
+    "url": "/clego-deploy/static/js/main.49e08687.chunk.js"
+  },
+  {
+    "revision": "b87ed350472146f8242c",
+    "url": "/clego-deploy/static/js/runtime-main.8ccd54ce.js"
+  }
+]);
